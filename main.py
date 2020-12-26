42c829b60e20e041bb280238e99ae2a25815ea5f
Juan Quispe <jquispeh63@gmail.com>
2020-12-26 00:36:17 -0500

    Adds menu
    -Creates menu with option to search for a card (Currently, it prints dummy text)

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
-from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtWidgets import QApplication, QMainWindow, QAction
 import sys
 
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
+
+        #Add menu options
+        mnbMainMenu = self.menuBar()
+
+        actSearcher = QAction("Search", self)
+        actSearcher.triggered.connect(self.showSearcher)
+        mnbMainMenu.addAction(actSearcher)
+
+    def showSearcher(self):
+        print("Showing searcher")
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
