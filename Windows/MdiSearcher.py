e0e067680a63b043997dc4b7bf0e6b9d34af143c
Juan Quispe <jquispeh63@gmail.com>
2020-12-26 01:02:39 -0500

    Adds visual properties to Searcher
    -Sets size of search window
    -Adds basic utilities for searching

--- Windows/MdiSearcher.py
+++ Windows/MdiSearcher.py
@@ -1,4 +1,4 @@
-from PyQt5.QtWidgets import QMdiSubWindow
+from PyQt5.QtWidgets import QMdiSubWindow, QLabel, QLineEdit, QPushButton
 
 class MdiSearcher(QMdiSubWindow):
     isShown = False
@@ -6,3 +6,32 @@ class MdiSearcher(QMdiSubWindow):
     def __init__(self, parent):
         super().__init__(parent)
         MdiSearcher.isShown = True
+        self.setGeometry(0, 0, 16+300, 33+600)
+        self.setFrame()
+
+    def closeEvent(self, event):
+        MdiSearcher.isShown = False
+        event.accept()
+
+    def setFrame(self):
+        self.setWindowTitle(".:. Searcher .:.")
+
+        #Searcher
+        lblSearch = QLabel(self)
+        lblSearch.setText("Write the exact name of the card (English):")
+        lblSearch.resize(280, 10)
+        lblSearch.move(8+10, 25+10)
+
+        lieSearch = QLineEdit(self)
+        lieSearch.resize(200, 20)
+        lieSearch.move(8+10, 25+30)
+        lieSearch.setMaxLength(50)
+
+        def doSearch():
+            print(lieSearch.text())
+
+        btnSearch = QPushButton(self)
+        btnSearch.resize(70, 22)
+        btnSearch.move(8+220, 25+29)
+        btnSearch.setText("Search")
+        btnSearch.clicked.connect(doSearch)
